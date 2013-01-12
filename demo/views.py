c82d058abe2ca3c6134e86a789f8a8d7be4b61ba
hirokiky <hirokiky@gmail.com>
2013-01-12 15:04:21 +0900

    Added demo

---
+++ demo/views.py
@@ -0,0 +1,19 @@
+from django.http.response import HttpResponse
+from django.core.urlresolvers import reverse
+
+from beautifulpredicaties.predicaties import RequestParamPredicate
+from beautifulpredicaties.views import PredicateProcessView
+
+
+class PonyView(PredicateProcessView):
+    dispatch_config = {'get_corn': RequestParamPredicate('corn'),
+                       'get_corn_1': RequestParamPredicate('corn=1'),
+                      }
+    def get_corn(self, request, *args, **kwargs):
+        return HttpResponse('pony with some corn' + reverse('pony'))
+
+    def get_corn_1(self, request, *args, **kwargs):
+        return HttpResponse('pony with unicorn')
+
+    def get_default(self, request, *args, **kwargs):
+        return HttpResponse('pony')
